d1b89efa81f843dc6040333865c2ab8163df9ce5
ALYK <alyk74@gmail.com>
2026-05-08 14:55:36 +0200

    Add @AndroidEntryPoint to MainActivity for Hilt injection

--- app/src/main/java/com/example/test23/MainActivity.kt
+++ app/src/main/java/com/example/test23/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.test23.navigation.Test23App
 import com.example.test23.ui.theme.Test23Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
